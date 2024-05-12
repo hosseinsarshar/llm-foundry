4ba674dd95e82aae44cd6deb091b008ba47a4f44
Hossein Sarshar <hosseins@google.com>
2024-05-11 22:49:47 -0400

    a

--- llmfoundry/data/utils.py
+++ llmfoundry/data/utils.py
@@ -141,6 +141,7 @@ def get_text_collator(
 ) -> Tuple[Union[transformers.DataCollatorForLanguageModeling,
                  ConcatenatedSequenceCollatorWrapper], int]:
     dataset_cfg = dataloader_cfg.get('dataset')
+    print(f'{dataset_cfg=}')
     assert isinstance(dataset_cfg, dict)
     eos_token_id = dataset_cfg.get('eos_token_id', None)
     bos_token_id = dataset_cfg.get('bos_token_id', None)
